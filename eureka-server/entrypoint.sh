65491fc77414da3f7ce15d38bfc88555391e4119
陈伟东 <2511055509@qq.com>
2021-07-06 10:42:25 +0800

    ~

--- eureka-server/entrypoint.sh
+++ eureka-server/entrypoint.sh
@@ -6,7 +6,7 @@ echo "unzip /maven-repository/repository.zip -d /root/.m2/"
 unzip /maven-repository/repository.zip -d /root/.m2/
 
 echo "mv -f /spring-cloud-ergouzi/eureka-server/settings-docker.xml  /root/.m2/settings-docker.xml"
-mv -f /spring-cloud-ergouzi/eureka-server/settings-docker.xml  /root/.m2/settings-docker.xml
+mv -f /spring-cloud-ergouzi/settings-docker.xml  /root/.m2/settings-docker.xml
 echo "rm -rf src/main/resources/application.yml"
 echo "cp -a /spring-cloud-ergouzi/application.yml /spring-cloud-ergouzi/eureka-server/src/main/resources/application.yml"
 rm -rf src/main/resources/application.yml
