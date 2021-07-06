4bc1a08e101aefe3dd31a5027506605f5a6e0890
陈伟东 <2511055509@qq.com>
2021-07-06 14:33:40 +0800

    ~

--- eureka-producer/entrypoint.sh
+++ eureka-producer/entrypoint.sh
@@ -6,14 +6,9 @@ rm -rf /root/.m2/repository
 echo "unzip /maven-repository/repository.zip -d /root/.m2/"
 unzip /maven-repository/repository.zip -d /root/.m2/
 
-echo "mv -f /spring-cloud-ergouzi/settings-docker.xml  /root/.m2/settings-docker.xml"
-mv -f /spring-cloud-ergouzi/settings-docker.xml  /root/.m2/settings-docker.xml
+#echo "mv -f /spring-cloud-ergouzi/settings-docker.xml  /root/.m2/settings-docker.xml"
+#mv -f /spring-cloud-ergouzi/settings-docker.xml  /root/.m2/settings-docker.xml
 
-
-#echo "rm -rf src/main/resources/application.yml"
-#rm -rf src/main/resources/application.yml
-#echo "mv bootstrap.properties src/main/resources/bootstrap.properties"
-#mv bootstrap.properties src/main/resources/bootstrap.properties
 
 cd ..
 
--- eureka-server/entrypoint.sh
+++ eureka-server/entrypoint.sh
@@ -8,10 +8,6 @@ unzip /maven-repository/repository.zip -d /root/.m2/
 #echo "mv -f /spring-cloud-ergouzi/eureka-server/settings-docker.xml  /root/.m2/settings-docker.xml"
 #mv -f /spring-cloud-ergouzi/settings-docker.xml  /root/.m2/settings-docker.xml
 
-#echo "rm -rf src/main/resources/application.yml"
-#echo "cp -a /spring-cloud-ergouzi/application.yml /spring-cloud-ergouzi/eureka-server/src/main/resources/application.yml"
-#rm -rf src/main/resources/application.yml
-#mv /spring-cloud-ergouzi/eureka-server/application.yml src/main/resources/application.yml
 
 echo "mvn -f pom.xml clean package"
 mvn -f pom.xml clean package
